b22ec4ea0d10e5441cfce0820e07d934d583844d
Diwakar Baidya <diwakarbaidya@BluePis-MacBook-Pro.local>
2025-01-22 03:16:31 +0530

    initial commit

---
+++ src/main/java/com/example/reportserver/ReportserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reportserver/ReportserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
