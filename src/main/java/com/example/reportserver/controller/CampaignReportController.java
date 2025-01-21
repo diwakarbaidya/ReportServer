c6d0630f8a03322f68a0b0af1d21b02e748b1b03
Diwakar Baidya <diwakarbaidya@BluePis-MacBook-Pro.local>
2025-01-22 04:19:27 +0530

    Final

---
+++ src/main/java/com/example/reportserver/controller/CampaignReportController.java
@@ -0,0 +1,24 @@
+package com.example.reportserver.controller;
+
+import com.example.campaign.model.Campaign;
+import com.example.campaign.service.CampaignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/campaignReport")
+public class CampaignReportController {
+    private final CampaignService campaignService;
+
+    public CampaignController(CampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    @GetMapping
+    public List<Campaign> getAllCampaigns() {
+        return campaignService.getAllCampaigns();
+    }
+}
--- src/test/java/com/example/reportserver/ReportserverApplicationTests.java
+++ src/test/java/com/example/reportserver/ReportserverApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.reportserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class ReportserverApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootApplication
+public class ReportserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignServiceApplication.class, args);
+    }
 }
+
